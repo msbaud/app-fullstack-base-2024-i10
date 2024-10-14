80cbe7143d30656dbd6e6567b004aa1adefbdbce
Martín Baudino <martinbaudino@gmail.com>
2024-10-14 08:01:19 -0300

    Agrega comentarios sobre los dispositivos.

--- src/frontend/ts/device.ts
+++ src/frontend/ts/device.ts
@@ -1,7 +1,15 @@
+/**
+ * Clase Device que representa un dispositivo.
+ * - id: Identificador del dispositivo. Lo asigna la base de datos.
+ * - name: Nombre del dispositivo. Lo asigna el usuario.
+ * - description: Descripción del dispositivo. Lo asigna el usuario.
+ * - state: Estado del dispositivo. Lo asigna el usuario.
+ * - type: Tipo de dispositivo. Lo asigna el usuario.
+ */
 class Device {
   public id: number;
   public name: string;
   public description: string;
-  public state: boolean;
+  public state: number;
   public type: number;
 }
