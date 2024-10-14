e4de0a983b69bcc11417451e061f6ceff7393dce
Martín Baudino <martinbaudino@gmail.com>
2024-10-14 03:47:22 -0300

    Agrega rutas para borrar un dispositivo por ID, a través de consulta SQL.

--- src/backend/index.js
+++ src/backend/index.js
@@ -13,6 +13,20 @@ app.use(express.static("/home/node/app/static/"));
 
 //=======[ Main module code ]==================================================
 
+app.post("/device/", function (req, res) {
+  utils.query(
+    "update Devices set state=" + req.body.status + " where id=" + req.body.id,
+    (err, resp, meta) => {
+      if (err) {
+        console.log(err.sqlMessage);
+        res.status(409).send(err.sqlMessage);
+      } else {
+        res.send("ok " + resp);
+      }
+    }
+  );
+});
+
 app.get("/device/:id", function (req, res) {
   utils.query(
     "SELECT id,description FROM Devices where id=" + req.params.id,
@@ -25,6 +39,24 @@ app.get("/device/:id", function (req, res) {
     }
   );
 });
+
+app.delete("/device/:id", function (req, res) {
+  const deviceId = req.params.id;
+  utils.query(
+    "DELETE FROM Devices WHERE id = ?",
+    [deviceId],
+    (error, respuesta) => {
+      if (error) {
+        res.status(409).send(error.sqlMessage);
+      } else {
+        res
+          .status(200)
+          .send({ message: "Dispositivo eliminado correctamente" });
+      }
+    }
+  );
+});
+
 app.get("/usuario", function (req, res) {
   res.send("[{id:1,name:'mramos'},{id:2,name:'fperez'}]");
 });
@@ -38,20 +70,6 @@ app.post("/usuario", function (req, res) {
     let mensaje = { mensaje: "El id o el name no estaban cargados" };
     res.status(400).send(JSON.stringify(mensaje));
   }
-});
-
-app.post("/device/", function (req, res) {
-  utils.query(
-    "update Devices set state=" + req.body.status + " where id=" + req.body.id,
-    (err, resp, meta) => {
-      if (err) {
-        console.log(err.sqlMessage);
-        res.status(409).send(err.sqlMessage);
-      } else {
-        res.send("ok " + resp);
-      }
-    }
-  );
 });
 
 app.get("/devices/", function (req, res, next) {
@@ -68,7 +86,7 @@ app.get("/devices/", function (req, res, next) {
 });
 
 app.listen(PORT, function (req, res) {
-  console.log("NodeJS API running correctly");
+  console.log("NodeJS API funciona correctamente");
 });
 
 //=======[ End of file ]=======================================================
